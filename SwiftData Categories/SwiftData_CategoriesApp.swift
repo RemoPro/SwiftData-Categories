db4f093fe1cf5158de92a123c2f57ee31f119589
Remo Prozzillo <55060292+RemoPro@users.noreply.github.com>
2025-04-24 20:53:10 +0200

    Initial Commit

---
+++ SwiftData Categories/SwiftData_CategoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_CategoriesApp.swift
+//  SwiftData Categories
+//
+//  Created by Remo Prozzillo on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_CategoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
