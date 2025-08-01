fac26e60e75828fd6bfb8e13c92500aaa59a4638
Remo Prozzillo <55060292+RemoPro@users.noreply.github.com>
2025-08-01 15:17:24 +0200

    Added Menu in AllItemsView for editing and deleting an item

--- SwiftData Categories/Views/AllItemsView.swift
+++ SwiftData Categories/Views/AllItemsView.swift
@@ -13,11 +13,12 @@ import SwiftData
 struct AllItemsView: View {
     @Query(sort: \Item.name) var items: [Item]
     @Environment(\.modelContext) var modelContext
-    @State private var showSheetAddCategory = false
-    @State private var showSheetEditCategory = false
+//    @State private var showSheetAddItem = false
+    @State private var editingItem: Item?
     
     var body: some View {
         NavigationStack {
+            
             List {
                 ForEach(items) { item in
                     VStack(alignment: .leading) {
@@ -25,9 +26,30 @@ struct AllItemsView: View {
                         Text(item.category?.name ?? "")
                             .font(.caption)
                     }
-                }
-                //TODO: contextMenu for editing and delete
+                    .contextMenu {
+                        // Edit
+                        Button("Edit", systemImage: "pencil", action: {
+                            // triggers the Edit sheet to open it
+                            editingItem = item
+                        })
+                        
+                        // Delete
+                        Button(role: .destructive) {
+                            modelContext.delete(item)
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                    } // contextMenu
+                } // ForEach
             } // List
+            .sheet(item: $editingItem) { item in
+                EditItemView(item: item)
+#if os(iOS)
+                    .presentationDetents([.height(170)])
+#elseif os(macOS)
+                    .frame(height: 150)
+#endif
+            } // sheet
             .navigationTitle("All Items")
         }
     }
